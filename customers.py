5d660c1d79f0938b223fa40726f5d215c23c1912
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-04 12:14:31 -0700

    Fixed login link, started Customer class

--- customers.py
+++ customers.py
@@ -3,5 +3,9 @@
 
 class Customer(object):
     """Ubermelon customer."""
-
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
     # TODO: need to implement this
