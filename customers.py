f4951e21fe43200209a97352ef8c026d4df81bdf
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-04 12:44:41 -0700

    Added read_customer and get_by_email functs

--- customers.py
+++ customers.py
@@ -9,3 +9,43 @@ class Customer(object):
         self.email = email
         self.password = password
     # TODO: need to implement this
+
+    def __repr__(self):
+        """Convenience method to show information about customer in console."""
+
+        return "<Customer: {}, {}, {}, {}>".format(self.first_name, 
+                self.last_name, self.email, self.password)
+
+def read_customer(filepath):
+
+    customer = {}
+
+    with open(filepath) as file:  # automatically closes the file
+        for line in file:
+            (first_name,
+             last_name,
+             email,
+             password) = line.rstrip().split("|")
+    
+            customer[email] = Customer(first_name,
+                                       last_name,
+                                       email,
+                                       password)
+
+            # can use: customer[email] = Customer(*line.rstrip().split("|"))
+
+    return customer
+
+
+def get_by_email(email):
+
+    return customer[email]
+
+
+
+
+
+
+customer = read_customer("customers.txt")
+
+
